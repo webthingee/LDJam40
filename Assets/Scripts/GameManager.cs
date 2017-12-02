4ab0704652e893484fcae0e960f6227d0342ab6f
Sean Lange <seanlange@SRLMacBook-558.local>
2017-12-01 21:53:44 -0800

    saving

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour {
+
+    // Rad Load
+	public Text radLoadText;
+    public float radLoadMax;
+	public float radLoadValue;
+	
+	void Start () 
+	{
+		radLoadValue = radLoadMax;
+		radLoadText.text = radLoadValue.ToString();
+	}
+	
+	void Update () 
+	{
+		radLoadValue -= Time.deltaTime;
+		radLoadText.text = Mathf.FloorToInt(radLoadValue).ToString();
+	}
+
+	void GameTimer ()
+	{
+
+	}
+}
---
+++ Assets/Scripts/RadEffectCtrl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadEffectCtrl : MonoBehaviour {
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		GameObject.Find("Master Manager").GetComponent<GameManager>().radLoadValue += 99;
+		Debug.Log("Master");
+	}
+	
+}
